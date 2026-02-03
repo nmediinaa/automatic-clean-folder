b7aa3823c1ec9747bb5ad92df64286a8b35b3ee6
nmedina <nicolascostam23@gmail.com>
2026-02-02 23:53:20 -0300

    :sparkles: feat: Add a FileMapper class to list all files from a directory

---
+++ automatic-clean-folder/automatic-clean-folder/FileMapper.cs
@@ -0,0 +1,21 @@
+﻿namespace automatic_clean_folder;
+
+public class FileMapper
+{
+    public List<FileInfo> Files { get; set; }
+    public string Path { get; set; }
+
+
+    public FileMapper(string path)
+    {
+        this.Path = path;
+    }
+
+    public List<FileInfo> GetFiles()
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(Path);
+        Files = directoryInfo.GetFiles("*.*").ToList();
+        
+        return Files;
+    }
+}
