4bece8fdaea853ab17213d052895f202ebe88957
nmediinaa <nicolascostam23@gmail.com>
2026-02-09 12:27:48 -0300

    :sparkles: feat: Add a LogWriter to handle log files output

---
+++ automatic-clean-folder/automatic-clean-folder/LogWriter.cs
@@ -0,0 +1,22 @@
+﻿namespace automatic_clean_folder;
+
+public static class LogWriter
+{
+   private static readonly string _path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + 
+                           @$"\temporaria\logs{DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")}.txt";
+   
+    public static void Write(string logMessage)
+    {
+        using var stream = new FileStream(_path, FileMode.Append);
+        using var writer = new  StreamWriter(stream);
+        
+        writer.WriteLine($"{logMessage} | {DateTime.Now}");
+        writer.Close();
+    }
+
+    public static string GetLogPath()
+    {
+        return Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + 
+                @$"\temporaria";
+    }
+}
