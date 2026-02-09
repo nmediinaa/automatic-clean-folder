eaaacb354b1b42c5953c466170c2201a87f7ce5b
nmediinaa <nicolascostam23@gmail.com>
2026-02-09 14:19:52 -0300

    :sparkles: feat: integrate LogWriter class into program

--- automatic-clean-folder/automatic-clean-folder/Program.cs
+++ automatic-clean-folder/automatic-clean-folder/Program.cs
@@ -6,6 +6,9 @@ using var factory = LoggerFactory.Create(builder => builder.AddConsole());
 var logger = factory.CreateLogger<Program>();
 
 logger.LogInformation("Program has started");
+
+LogWriter.Write("[INFO] The Program has started");
+
 Thread.Sleep(1000);
 
 Menu menu = new Menu();
